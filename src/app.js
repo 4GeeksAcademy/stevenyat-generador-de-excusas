aa76b12629874a179618f9e7c7787860cf53c89b
Steven Acero <stevenacero07@gmail.com>
2025-10-26 05:49:47 +0000

    proyecto

--- src/app.js
+++ src/app.js
@@ -7,5 +7,25 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let who = ['El perro', 'Mi abuela', 'El cartero', 'Mi pajaro'];
+
+  let action = ['se comio', 'orino', 'choco', 'rompio'];
+
+  let what = ['mi tarea', 'mi telefono', 'el carro'];
+
+  let when = ['antes de clase', 'mientras dormia', 'minetras hacia ejercicio', 'durante el almuezo', 'Mientras estaba orando'];
+
+  let whoAleatorio = seleccionarAleatorio(who);
+  let actionAleatorio = seleccionarAleatorio(action);
+  let whatAleatorio = seleccionarAleatorio(what);
+  let whenAleatorio = seleccionarAleatorio(when);
+  let excuseElement = this.document.querySelector("#excusas");
+  excuseElement.innerHTML = `${whoAleatorio} ${actionAleatorio} ${whatAleatorio} ${whenAleatorio}`;
 };
+function seleccionarAleatorio(lista){
+
+  let elementoAleatorio = Math.floor(Math.random() * lista.length);
+  return lista[elementoAleatorio]
+}
+console.log
